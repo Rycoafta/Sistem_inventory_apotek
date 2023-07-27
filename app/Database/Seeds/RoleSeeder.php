5cf42f22bdaaee884826040d8d8c74f087384ce0
BobbyJulianAkbar <bobby.120140082@student.itera.ac.id>
2023-07-27 22:23:59 +0700

    Update 3

--- app/Database/Seeds/RoleSeeder.php
+++ app/Database/Seeds/RoleSeeder.php
@@ -9,8 +9,15 @@ class RoleSeeder extends Seeder
     public function run()
     {
         $role = [
-            ['id' => 1, 'nama' => 'A'],
-            ['id' => 2, 'nama' => 'B'],
+            ['id' => 1, 'nama' => 'admin'],
+            ['id' => 2, 'nama' => 'gm'],
+            ['id' => 3, 'nama' => 'manager'],
+            ['id' => 4, 'nama' => 'supervisor'],
+            ['id' => 5, 'nama' => 'gm_bd'],
+            ['id' => 6, 'nama' => 'manager_bd'],
+            ['id' => 7, 'nama' => 'supervisor_bd'],
+            ['id' => 8, 'nama' => 'staff_warehouse'],
+            ['id' => 9, 'nama' => 'staff'],
             ];
         $this->db->table('role')->insertBatch($role);
     }

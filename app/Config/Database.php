90136141338cdbee4736846f5fa0baa8aa124f4a
BobbyJulianAkbar <bobby.120140082@student.itera.ac.id>
2023-11-02 18:33:16 +0700

    Update 1

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'sistem-inventory',
+        'database' => 'sistem-inventory-apotek',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
